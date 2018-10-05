4d855574cde00a951cceee4244d6ed03116f6bad
dharmateja <tdharma591@gmail.com>
2018-10-05 11:18:57 -0700

    ads-module3
    activity-8

---
+++ practice-m1/ads-module3/ads-module3-activity8/triplet.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import threesum.Threesum;
+public class triplet {
+	public static void main(String[] args) {
+		Threesum ts=new Threesum();
+		int n;
+		System.out.println("Enter size:");
+		Scanner sc = new Scanner(System.in);
+		n=sc.nextInt();
+        int a[]=new int[n];
+        System.out.println("Enter an array:");
+        for(int i=0;i<n;i++)
+        {
+			a[i]=sc.nextInt();
+        }
+        Arrays.sort(a);
+        ts.search(a);
+	}
+}
