646a1e99aeca89f9dee808fcf5834b37220af9c1
dharmateja <tdharma591@gmail.com>
2018-10-05 14:19:14 -0700

    module 3
    assignment-1

--- practice-m1/ads-module3/ads-module3-activity8/threesum/Threesum.java
+++ practice-m1/ads-module3/ads-module3-activity8/threesum/Threesum.java
@@ -1,23 +1,28 @@
 package threesum;
+import java.util.*;
 public class Threesum 
 {
   public void search(int arr[])
   {
-	  
+	  int count=0,x;
+	  System.out.println("Enter any value:");
+	  Scanner sc=new Scanner(System.in);
+	  x=sc.nextInt();
 	  for(int i=0;i<arr.length-2;i++)
 	  {
 		  for(int j=i+1;j<arr.length-1;j++)
 		  {
 			  for(int k=j+1;k<arr.length;k++)
 			  {
-				  if(arr[i]+arr[j]+arr[k]==0)
+				  if(arr[i]+arr[j]+arr[k]==x)
 				  {
-					  System.out.println(arr[i]+" "+arr[j]+" "+arr[k]);
+					  //System.out.println(arr[i]+" "+arr[j]+" "+arr[k]);
 					  //break;
+					  count++;
 				  }  
 			  }
 		  }
 	  }
-	  
+	 System.out.println(count); 
   }
 }
