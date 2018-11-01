5decb802601bf7418321b6eb3bb71d7cfd947d99
dharmateja <tdharma591@gmail.com>
2018-10-31 17:07:16 -0700

    module12,14

---
+++ ads-module12/Solution.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+class MinHeap
+{
+	public boolean Heap(Comparable a[])
+    {
+        for(int i=0;i<a.length;i++)
+        {
+            if(a.length>2*i+1&& a.length>2*i+2)
+            {
+                if(a[i].compareTo(a[2*i+1])>0 || a[i].compareTo(a[2*i+2])>0)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;	
+	}
+}
+class Solution 
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int size;
+		String datatype=sc.nextLine();
+		int n=sc.nextInt();
+		MinHeap mh=new MinHeap();
+		sc.nextLine();
+		for(int i=0;i<n;i++)
+		{
+			String line=sc.nextLine();
+			if(line.length()==0)
+			{
+				System.out.println("false");
+			}
+			else
+			{
+				String input[]=line.split(",");
+				switch(datatype)
+				{
+				case "String":size=0;
+								String sheap[]=new String[input.length];
+								for(int j=0;j<input.length;j++)
+								{
+									sheap[size++]=input[j];
+								}
+								System.out.println(mh.Heap(sheap));
+								break;
+				case "Float":
+								size=0;
+								Float fheap[]=new Float[input.length];
+								for(int j=0;j<input.length;j++)
+								{
+									fheap[size++]=Float.parseFloat(input[j]);
+								}
+								System.out.println(mh.Heap(fheap));
+								break;
+				case "Double":
+								size=0;
+								Double dheap[]=new Double[input.length];
+								for(int j=0;j<input.length;j++)
+								{
+									dheap[size++]=Double.parseDouble(input[j]);
+								}
+								System.out.println(mh.Heap(dheap));
+								break;
+				case "Integer":
+								size=0;
+								Integer iheap[]=new Integer[input.length];
+								for(int j=0;j<input.length;j++)
+								{
+									iheap[size++]=Integer.parseInt(input[j]);
+								}
+								System.out.println(mh.Heap(iheap));
+								break;
+					
+				}
+				
+			}
+			
+		}
+		
+	}
+}
